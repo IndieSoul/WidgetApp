7d927565c5f4e856d7c8b4625823eaf13785a010
Luis Enrique Rosas Espinoza <enriquerosas@lesscoded.dev>
2023-05-05 15:30:31 -0600

    Initial Commit

---
+++ WidgetApp/WidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetAppApp.swift
+//  WidgetApp
+//
+//  Created by Luis Enrique Rosas Espinoza on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
